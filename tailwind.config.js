d551a68231686fe3b6924ffcf07d6da078dadb06
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2023-07-29 15:50:45 +0200

    beginning

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Dosis:['Dosis', 'sans-serif']
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
