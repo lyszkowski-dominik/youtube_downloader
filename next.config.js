deab3704a86652291c1bd0364e9edcb1bf06d646
lyszkowski-dominik <57920532+lyszkowski-dominik@users.noreply.github.com>
2023-07-29 21:12:56 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  env: {
-    YT_API_KEY: 'bbb7ba258amsh5e926b23c1758abp17b95fjsnbac16932bd24',
-  },
-};
-
-module.exports = nextConfig;
